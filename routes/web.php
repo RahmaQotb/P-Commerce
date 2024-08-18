c7db83ab53ee24ea52906fb92247cd45aaa8c10f
Abdelrhman Aiiad <abdelrhman.aiiad@gmail.com>
2024-08-18 10:23:18 +0200

    another

--- routes/web.php
+++ routes/web.php
@@ -155,7 +155,7 @@ Route::controller(OrderController::class)->middleware("auth:admin")->prefix("adm
 Route::controller(UserOrderController::class)->middleware("auth")->group(function(){
     Route::get("orders","myOrders");
     Route::get("orders/{id}","show");
-    Route::post("make_order","makeOrder")->name("makeOrder");
+    Route::post("make_order","makeOrder")->name("makeOrder"); 
 });
 
 Route::controller(CartController::class)->middleware("auth")->group(function(){
